16cc95f9695481b506b1bd2d24dda6e2ef5601b5
Qian Li <40520052+futureisatyourhand@users.noreply.github.com>
2020-11-04 11:35:07 +0800

    Add files via upload

---
+++ Sales_data.h
@@ -0,0 +1,10 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+#include<iostream>
+#include<string>
+struct Sales_data{
+    string bookNo;
+    unsigned uints_sold=0;
+    double revenue=0.0;
+};
+#endif
